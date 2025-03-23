07a8a724a9a42be4e9932bc8a5dd2b387a7204e7
Lolgamer303 <nathanchaps@icloud.com>
2025-03-23 17:21:26 +0100

    added content

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,23 +1,59 @@
+import { getServerSession } from "next-auth";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function Header() {
+export default async function Header() {
+  const session = await getServerSession();
   return (
-    <div className="absolute border-b-2 border-custom-border top-0 left-0 w-full text-custom-text-primary p-4 flex justify-between items-center">
-      <div className="text-3xl">
+    <div className="absolute border-b-2 border-custom-border top-0 left-0 w-full text-custom-text-primary pt-4 px-4 flex justify-between items-center">
+      <div className="text-3xl border-r-2 border-custom-border pr-3">
         <h1>RoleplayChat</h1>
       </div>
       <div className="flex-grow text-center text-2xl font-bold">
-        <Link href='' className='pl-3'>Home</Link>
-        <Link href=''>Chat</Link>
-        <Link href=''>API Docs</Link>
-        <Link href=''>API Keys</Link>
+        <Link href="" className="px-7 border-r-2 border-custom-border text-custom-text-secondary">
+          Home
+        </Link>
+        <Link href="" className="px-7 border-r-2 border-custom-border text-custom-text-secondary">Chat</Link>
+        <Link href="" className="px-7 border-r-2 border-custom-border text-custom-text-secondary">API Docs</Link>
+        <Link href="" className="px-7 text-custom-text-secondary">API Keys</Link>
       </div>
-      <Link
-        href="api/auth/signin"
-        className=" bg-custom-background-secondary border-1 border-custom-border items-center justify-center flex hover:border-custom-text-primary text-custom-text-primary rounded-md text-[25px] duration-200 hover:bg-custom-hover active:bg-custom-click active:scale-90 hover:scale-110 hover:rotate-6 w-24 h-12"
-      >
-        Login
-      </Link>
+      <div className="flex justify-end border-l-2 border-custom-border pl-3">
+        {session ? (
+          <div className="flex items-center">
+            {session.user.image ? (
+              <Image
+                className="rounded-full"
+                src={session.user.image}
+                alt="User Image"
+                width={40}
+                height={40}
+              />
+            ) : (
+              <Image
+                className="rounded-full"
+                src="https://lh3.googleusercontent.com/a/default-user"
+                alt="User Image"
+                width={40}
+                height={40}
+              />
+            )}
+            <p className="pl-2">{session.user.name}</p>
+          </div>
+        ) : (
+          <LoginButton />
+        )}
+      </div>
     </div>
+  );
+}
+
+function LoginButton() {
+  return (
+    <Link
+      href="api/auth/signin"
+      className=" bg-custom-background-secondary border-1 border-custom-border items-center justify-center flex hover:border-custom-text-primary text-custom-text-primary rounded-md text-[25px] duration-200 hover:bg-custom-hover active:bg-custom-click active:scale-90 hover:scale-110 hover:rotate-6 w-24 h-12"
+    >
+      Login
+    </Link>
   );
 }
